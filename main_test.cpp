fee563c31a501c89f7c2cd3ab6457da080dea940
DrWhilson <DrWhilsonMFI@gmail.com>
2023-11-17 10:35:38 +0700

    Finaly, found GTest

--- main_test.cpp
+++ main_test.cpp
@@ -1,10 +1,42 @@
 #include "lab1.h"
-#include <CppUTest/CommandLineTestRunner.h>
-#include <CppUTest/TestHarness.h>
-#include <CppUTest/UtestMacros.h>
+#include <gtest/gtest.h>
+#include <math.h>
 
-TEST_GROUP(FirstGroup){};
+double squareRoot(const double a) {
+  double b = sqrt(a);
+  if (b != b) { // nan check
+    return -1.0;
+  } else {
+    return sqrt(a);
+  }
+}
 
-TEST(FirstGroup, failtst) { FAIL("FAIL!"); }
+TEST(SquareRootTest, PositiveNos) {
+  ASSERT_EQ(6, squareRoot(36.0));
+  ASSERT_EQ(18.0, squareRoot(324.0));
+  ASSERT_EQ(25.4, squareRoot(645.16));
+  ASSERT_EQ(0, squareRoot(0.0));
+}
 
-int main(int argc, char **argv) { return RUN_ALL_TESTS(argc, argv); }
+TEST(SquareRootTest, NegativeNos) {
+  ASSERT_EQ(-1.0, squareRoot(-15.0));
+  ASSERT_EQ(-1.0, squareRoot(-0.2));
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
+// #include "lab1.h"
+// #include <CppUTest/CommandLineTestRunner.h>
+// #include <CppUTest/MemoryLeakDetector.h>
+// #include <CppUTest/MemoryLeakDetectorNewMacros.h>
+// #include <CppUTest/TestHarness.h>
+// #include <CppUTest/UtestMacros.h>
+//
+// TEST_GROUP(FirstGroup){};
+//
+// TEST(FirstGroup, failtst) { FAIL("FAIL!"); }
+//
+// int main(int argc, char **argv) { return RUN_ALL_TESTS(argc, argv); }
