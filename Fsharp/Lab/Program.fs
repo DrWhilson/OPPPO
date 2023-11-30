61302ec4c3619fb95bdc8ddf52679205c4685253
DrWhilson <DrWhilsonMFI@gmail.com>
2023-11-30 15:41:05 +0700

    add a file read

--- Fsharp/Lab/Program.fs
+++ Fsharp/Lab/Program.fs
@@ -1,2 +1,16 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+open System.IO
+
+let readLinesFromFile (filePath: string) =
+    let lines = ref []
+    use reader = new StreamReader(filePath)
+
+    while not reader.EndOfStream do
+        lines := reader.ReadLine() :: !lines
+
+    List.rev !lines
+
+let filePath = "./input"
+let lines = readLinesFromFile filePath
+printfn "Содержимое файла %s:" filePath
+lines |> List.iter (fun line -> printfn "%s" line)
