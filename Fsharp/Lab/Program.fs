0b90dca3849e3726a222de39fd1c8c4553d23283
DrWhilson <DrWhilsonMFI@gmail.com>
2023-11-30 20:16:48 +0700

    fix .gitignore

--- Fsharp/Lab/Program.fs
+++ Fsharp/Lab/Program.fs
@@ -1,5 +1,34 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
+﻿open System
 open System.IO
+open System.Collections.Generic
+open FSharpx.Collections.Experimental
+
+type Point3D =
+    val X: int
+    val Y: int
+    val Z: int
+
+type Figure() =
+    let mutable Name = ""
+    let mutable Density = 0.0
+
+type Sphere() =
+    inherit Figure()
+
+    let mutable Radius = 0
+
+type Parall() =
+    inherit Figure()
+
+    let mutable edgeA = 0
+    let mutable edgeB = 0
+    let mutable edgeC = 0
+
+type Cyll() =
+    inherit Figure()
+
+    let mutable Heigh = 0
+    let mutable Radius = 0
 
 let readLinesFromFile (filePath: string) =
     let lines = ref []
